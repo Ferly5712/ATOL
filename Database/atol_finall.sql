14071108312cb4efd92f3f459081f6ccf9c6f8f5
DebbyMVP <a7xrockets@yahoo.com>
2016-07-28 16:56:52 +0700

    -database

--- Database/atol_finall.sql
+++ Database/atol_finall.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50141
 File Encoding         : 65001
 
-Date: 2016-07-18 13:29:35
+Date: 2016-07-28 16:56:26
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -34,8 +34,14 @@ CREATE TABLE `jadwal` (
 -- ----------------------------
 -- Records of jadwal
 -- ----------------------------
-INSERT INTO `jadwal` VALUES ('BJ001', 'BDG01', 'JKT01', '00:00:00', '09:30:00', '0');
+INSERT INTO `jadwal` VALUES ('BJ001', 'BDG01', 'JKT01', '07:00:00', '09:00:00', '1300000');
 INSERT INTO `jadwal` VALUES ('BJ002', 'BDG02', 'JKT01', '08:00:00', '10:20:00', '135000');
+INSERT INTO `jadwal` VALUES ('BJ003', 'BDG01', 'JKT01', '07:30:00', '09:30:00', '1300000');
+INSERT INTO `jadwal` VALUES ('BJ004', 'BDG01', 'JKT01', '08:00:00', '10:00:00', '1300000');
+INSERT INTO `jadwal` VALUES ('BJ005', 'BDG01', 'JKT01', '08:30:00', '10:30:00', '1300000');
+INSERT INTO `jadwal` VALUES ('BJ006', 'BDG01', 'JKT01', '09:00:00', '11:00:00', '1300000');
+INSERT INTO `jadwal` VALUES ('BJ007', 'BDG01', 'JKT01', '10:00:00', '12:00:00', '1300000');
+INSERT INTO `jadwal` VALUES ('JB001', 'JKT01', 'BDG01', '07:00:00', '09:00:00', '1300000');
 
 -- ----------------------------
 -- Table structure for lokasi
@@ -69,13 +75,35 @@ CREATE TABLE `pelanggan` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`,`username`)
-) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 
 -- ----------------------------
 -- Records of pelanggan
 -- ----------------------------
 INSERT INTO `pelanggan` VALUES ('1', 'Arthur', 'arthur@chest.com', '087594562144', 'arthur', '68830aef4dbfad181162f9251a1da51b');
-INSERT INTO `pelanggan` VALUES ('2', 'Fox', 'asd@dfs.com', '08456874321', 'fox', '2b95d1f09b8b66c5c43622a4d9ec9a04');
+INSERT INTO `pelanggan` VALUES ('4', 'ceker', 'asd@sdf.com', '08465489745', 'ceker', '94636d1e282358814470eb9859f95f68');
+
+-- ----------------------------
+-- Table structure for pemesanan
+-- ----------------------------
+DROP TABLE IF EXISTS `pemesanan`;
+CREATE TABLE `pemesanan` (
+  `kd_pesan` int(11) NOT NULL AUTO_INCREMENT,
+  `kd_jadwal` char(5) NOT NULL,
+  `tanggal` date NOT NULL,
+  `jumlah` int(11) NOT NULL,
+  `total` int(11) NOT NULL,
+  `status` int(11) NOT NULL,
+  PRIMARY KEY (`kd_pesan`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+-- ----------------------------
+-- Records of pemesanan
+-- ----------------------------
+INSERT INTO `pemesanan` VALUES ('1', 'BJ003', '0000-00-00', '1', '1300000', '0');
+INSERT INTO `pemesanan` VALUES ('2', 'BJ006', '2016-07-29', '2', '2600000', '0');
+INSERT INTO `pemesanan` VALUES ('3', 'BJ003', '2016-07-29', '5', '6500000', '0');
+INSERT INTO `pemesanan` VALUES ('4', '', '0000-00-00', '0', '0', '0');
 
 -- ----------------------------
 -- Table structure for user
